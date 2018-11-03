09f58e85b0a65ac9404f38c4da4980a921780a39
alaska11 <44711789+alaska11@users.noreply.github.com>
2018-11-03 19:39:34 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $token = '789933109:AAGwpNJWVTPmH33XJyDPI1qYLFbhziRdo6o';
 
-function sendMessage
+function sendMessage($token, $id)
 {
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=hi. it is all i can(");
 }
