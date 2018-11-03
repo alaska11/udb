466557c05d4813c14b121046f9e285fcc20a18a3
alaska11 <44711789+alaska11@users.noreply.github.com>
2018-11-03 20:20:28 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -8,12 +8,13 @@ $output = json_decode(file_get_contents('php://input'),true);
 //$lastms = $output['message']['text'];
 $id = $output['message']['chat']['id'];
 $token = '789933109:AAGwpNJWVTPmH33XJyDPI1qYLFbhziRdo6o';
-function sendMessage($token, $id)
+$mes1 = 'hi, just can say hi';
+function sendMessage($token, $id, $mes1)
 {
-    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=hi. it is all i can(");
+    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=" . $mes1);
 }
-sendMessage($token, $id);
+sendMessage($token, $id, $mes1);
 //file_put_contents("logs.txt",$lastms);
 // 2 file_put_contents("logs.txt",$output);
-$h = fopen('logs.txt', 'r+');
-fwrite($h, var_export($output, true));
+$h = fopen('logs.txt', 'r+'); //выводит массив
+fwrite($h, var_export($output, true)); //выводит массив
