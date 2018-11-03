0a15ac633d53ed32be25b8f4b6329a31313a3c6d
alaska11 <44711789+alaska11@users.noreply.github.com>
2018-11-03 05:13:15 +0500

    Update index.php

--- index.php
+++ index.php
@@ -9,5 +9,5 @@
 $output = json_decode(file_get_contents('php://input'),true);
 $lastms = $output['message']['text'];
 $id = $output['message']['chat']['id'];
-file_get_contents("https://api.telegram.org/bot789933109:AAGwpNJWVTPmH33XJyDPI1qYLFbhziRdo6o/sendMessage?chat_id=".$id."&text=hi");
+file_get_contents("https://api.telegram.org/bot789933109:AAGwpNJWVTPmH33XJyDPI1qYLFbhziRdo6o/sendMessage?chat_id=".$id."&text=hi. it is all i can(");
 file_put_contents("logs.txt",$lastms);
